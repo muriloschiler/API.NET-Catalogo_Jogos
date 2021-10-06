8a1a5f33077b93fb528e6002ba42ecaf87cd737e
muriloschiler <muriloschiler.ls@gmail.com>
2021-10-06 08:46:50 -0300

    Estrutura para a geracao de relatorios

---
+++ API.NET-Catalogo_Jogos/API.NET-Catalogo_Jogos/Services/ReportService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.NET_Catalogo_Jogos.Services
+{
+    public class ReportService
+    {
+
+        public string BuildReport(string typeReport)
+        {
+
+        }
+    }
+}
